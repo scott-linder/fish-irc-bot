1b711a4b79061913bf26d75202a23acbf0024471
scott-linder <scott.b.linder@wmich.edu>
2014-10-07 15:17:35 -0400

    Expand help module to work with arbitrary topics

--- mods/help.fish
+++ mods/help.fish
@@ -1,1 +1,11 @@
-msg $chan "hello $nick, I am fish, the friendly interactive bot. I know how to: "(find mods/ -type f | sed -n 's#^mods/\(.*\).fish$#'$leader'\1#p' | tr \n ' ')
+if test -z $rest
+    msg $chan "hello $nick, I am fish, the friendly interactive bot. I know how to: "(find mods/ -type f -follow | sed -n 's#^mods/\(.*\).fish$#'$leader'\1#p' | tr \n ' ')
+else
+    set topic (echo $rest | tr -cd '[:alpha:]')
+    set file help/$topic.txt
+    if test -f $file
+        msg $chan $nick': '(cat $file)
+    else
+        msg $chan $nick": no help available for \"$topic\""
+    end
+end
