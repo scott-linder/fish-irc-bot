4aaf1f27789975ec3dc20aa456fe4eb638ab2207
scott-linder <scott.b.linder@wmich.edu>
2014-10-14 17:36:53 -0400

    Reword help message

--- mods/help.fish
+++ mods/help.fish
@@ -1,5 +1,5 @@
 if test -z $rest
-    msg $chan "hello $nick, I am fish, the friendly interactive bot. I know how to: "(find mods/ -type f -follow | sed -n 's#^mods/\(.*\).fish$#'$leader'\1#p' | tr \n ' ')'and I can help you with: '(find help/ -type f -follow | sed -n 's#^help/\(.*\).txt$#\1#p' | tr \n ' ')
+    msg $chan "Yes, $nick, this is fish. I can "(find mods/ -type f -follow | sed -n 's#^mods/\(.*\).fish$#'$leader'\1#p' | tr \n ' ')'and I can help you with: '(find help/ -type f -follow | sed -n 's#^help/\(.*\).txt$#\1#p' | tr \n ' ')
 else
     set topic (echo $rest | tr -cd '[:alpha:]')
     set file help/$topic.txt
