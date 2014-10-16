00271d320c5e8d16010e8e30e08943a5998bd158
scott-linder <scott.b.linder@wmich.edu>
2014-10-16 16:07:20 -0400

    Require admin privs for $rm

--- cmd/rm.fish
+++ cmd/rm.fish
@@ -1,7 +1,9 @@
 . lib/file.fish
 set file (file_file $rest)
 if test -n $file -a -f $file
-    rm $file
+    if contains $nick $ADMINS
+        rm $file
+    end
 else
     msg $chan $nick': no such file'
 end
