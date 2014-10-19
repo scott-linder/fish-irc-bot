f5e4e9524d0057efb6544f64e9f1549b5c4eb272
scott-linder <scott.b.linder@wmich.edu>
2014-10-19 13:24:38 -0400

    De-jsonify $rustc output

--- cmd/rustc.fish
+++ cmd/rustc.fish
@@ -1,1 +1,1 @@
-msg $chan $nick': '(echo '{"code":"'(echo $rest | sed 's/\\\\/\\\\\\\\/g' | sed 's/"/\\\\"/g')'"}' | curl -X POST -d @- -H 'Content-Type: application/json' 'http://play.rust-lang.org/evaluate.json')
+msg $chan (echo '{"code":"'(echo $rest | sed 's/\\\\/\\\\\\\\/g' | sed 's/"/\\\\"/g')'"}' | curl -X POST -d @- -H 'Content-Type: application/json' 'http://play.rust-lang.org/evaluate.json' | python3 -c 'import json; print(json.loads(input())["result"]);')
