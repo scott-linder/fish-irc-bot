c3374e599beb9cb511f06498d916692838ded1e4
scott-linder <scott.b.linder@wmich.edu>
2014-10-20 17:29:31 -0400

    Clarify the error message for $cat now that it is a default case

--- cmd/cat.fish
+++ cmd/cat.fish
@@ -3,5 +3,5 @@ set file (file_file $rest)
 if test -n $file -a -f $file
     msg $chan (cat $file | sort -R | head -1)
 else
-    msg $chan $nick': no such file'
+    msg $chan $nick': no such file or command'
 end
