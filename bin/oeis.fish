534fa97c6d6304fcb5b140da1b40d74a7be0c7f6
scott-linder <scott.b.linder@wmich.edu>
2014-10-25 20:36:30 -0400

    Add error handling to $oeis

--- bin/oeis.fish
+++ bin/oeis.fish
@@ -1,1 +1,6 @@
-msg $chan (curl "http://oeis.org/search?fmt=text&q=$rest" | sed -n 's/^%N \(.*\)/\1/p' | head -1)
+set s (curl "http://oeis.org/search?fmt=text&q=$rest" | sed -n 's/^%N \(.*\)/\1/p' | head -1)
+if test -n "$s"
+    msg $chan $nick': '$s
+else
+    msg $chan $nick': no such sequence'
+end
