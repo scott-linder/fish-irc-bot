6f0f7a3a4fd4005d669931b048f796b1ae17c806
scott-linder <scott.b.linder@wmich.edu>
2014-12-03 19:27:51 -0500

    Make $grep case insensitive

--- bin/grep.fish
+++ bin/grep.fish
@@ -3,7 +3,7 @@ set args (echo $rest | tr ' ' \n)
 set pattern $args[1]
 set file (file_file $args[2])
 if test -f "$file"
-    msg $chan (grep $pattern $file | sort -R | head -1)
+    msg $chan (grep -i $pattern $file | sort -R | head -1)
 else
     msg $chan $nick': no such file'
 end
