b5f4ebe1036a5799d6a0890fb724f4b49c229645
scott-linder <scott.b.linder@wmich.edu>
2014-12-26 13:10:34 -0500

    Enhance $grep to accept arguments and complex patterns

--- bin/grep.fish
+++ bin/grep.fish
@@ -1,9 +1,23 @@
 . lib/file.fish
-set args (echo $rest | tr ' ' \n)
-set pattern $args[1]
-set file (chan_file $chan $args[2])
-if test -f "$file"
-    msg $chan (grep -i $pattern $file | sort -R | head -1)
+. lib/opts.fish
+
+set opts (opts_getopts ci $rest)
+set sep (opts_find_sep $opts)
+set flags (opts_flags $sep $opts)
+set rest (opts_rest $sep $opts)
+
+set -l pattern (echo $rest[1..-2])
+set -l file (chan_file $chan $rest[-1])
+
+if test -n "$pattern" -a -n "$file" -a -f "$file"
+    switch $status
+        case 0
+            msg $chan (grep $flags $pattern $file | sort -R | head -1)
+        case 1
+            msg $chan $nick': no matching lines'
+        case '*'
+            msg $chan $nick': grep error'
+    end
 else
-    msg $chan $nick': no such file'
+    msg $chan $nick': invalid pattern or file'
 end
