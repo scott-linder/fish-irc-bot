e41f56628a0728f8f01509692f80e3c9f2a799fa
scott-linder <scott.b.linder@wmich.edu>
2015-01-11 17:17:22 -0500

    Make $rustc more user-friendly
    Wrap code in main() and a printf for Show

--- bin/rustc.fish
+++ bin/rustc.fish
@@ -1,1 +1,17 @@
-msg $chan (echo '{"code":"'(echo $rest | sed 's/\\\\/\\\\\\\\/g' | sed 's/"/\\\\"/g')'"}' | curl -X POST -d @- -H 'Content-Type: application/json' 'http://play.rust-lang.org/evaluate.json' | python3 -c 'import json; print(json.loads(input())["result"]);')
+set code "\
+#![feature(asm, non_ascii_idents, simd, thread_local, unsafe_destructor)]\
+#![allow(dead_code, unused_variables, unstable)]\
+extern crate arena;\
+extern crate libc;\
+extern crate regex;\
+fn main() {\
+    let r = {\
+        $rest\
+    };\
+    println!(\"{:?}\", r)\
+}\
+"
+msg $chan (echo $code \
+    | python3 -c 'import json; print(json.dumps({"code": input()}))' \
+    | curl -X POST -d @- -H 'Content-Type: application/json' 'http://play.rust-lang.org/evaluate.json' \
+    | python3 -c 'import json; print(json.loads(input())["result"]);')
