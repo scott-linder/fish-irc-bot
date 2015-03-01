07fac6ae139740f1570e93d91c8664ed64a8acf3
scott-linder <scott.b.linder@wmich.edu>
2015-03-01 14:43:51 -0500

    update $rustc

--- bin/rustc.fish
+++ bin/rustc.fish
@@ -1,17 +1,17 @@
 set code "\
-#![feature(asm, non_ascii_idents, simd, thread_local, unsafe_destructor)]\
-#![allow(dead_code, unused_variables, unstable)]\
-extern crate arena;\
+#![feature(asm, simd, thread_local, unsafe_destructor)]\
+#![feature(core, libc, collections, std_misc, io, path)]\
+#![allow(dead_code, unused_variables)]\
+#![allow(unused_features)]\
 extern crate libc;\
-extern crate regex;\
+fn show<T: std::fmt::Debug>(e: T) { println!(\"{:?}\", e) }\
 fn main() {\
-    let r = {\
+    show({\
         $rest\
-    };\
-    println!(\"{:?}\", r)\
+    });\
 }\
 "
 msg $chan (echo $code \
     | python3 -c 'import json; print(json.dumps({"code": input()}))' \
-    | curl -X POST -d @- -H 'Content-Type: application/json' 'http://play.rust-lang.org/evaluate.json' \
+    | curl --insecure -X POST -d @- -H 'Content-Type: application/json' 'https://play.rust-lang.org/evaluate.json' \
     | python3 -c 'import json; print(json.loads(input())["result"]);')
