a623aa30a51982c304db188d00fa608822a78b7c
scott-linder <scott.b.linder@wmich.edu>
2015-05-22 15:21:13 -0400

    Update $office to support listing registrations

--- bin/office.fish
+++ bin/office.fish
@@ -2,13 +2,14 @@
 
 set base_url "http://magpie.dhcp.io"
 
-set opts (opts_getopts r:d: $rest)
+set opts (opts_getopts r:d:l $rest)
 set sep (opts_find_sep $opts)
 set flags (opts_flags $sep $opts)
 set rest (opts_rest $sep $opts)
 
 set reg
 set dereg
+set list
 
 set i 1
 while test $i -le (count $flags)
@@ -19,6 +20,8 @@ while test $i -le (count $flags)
         case '-d'
             set i (math $i+1)
             set dereg $flags[$i]
+        case '-l'
+            set list 1
     end
     set i (math $i+1)
 end
@@ -27,6 +30,8 @@ if test -n "$reg"
     msg $chan $nick': '(curl -d "nick=$nick&mac=$reg" "$base_url/reg")
 else if test -n "$dereg"
     msg $chan $nick': '(curl -d "nick=$nick&mac=$dereg" "$base_url/dereg")
+else if test -n "$list"
+    msg $chan $nick': '(curl -d "nick=$nick" "$base_url/list")
 else
     set around (curl "$base_url/plain")
     if test -n "$around"
