8529d825d2c1b659bbc4214f1b1e18cea7ffc058
scott-linder <scott.b.linder@wmich.edu>
2015-05-22 15:21:25 -0400

    Add $o

---
+++ bin/o.fish
@@ -0,0 +1,38 @@
+. lib/color.fish
+. lib/opts.fish
+
+set opts (opts_getopts rc: $rest)
+set sep (opts_find_sep $opts)
+set flags (opts_flags $sep $opts)
+set rest (opts_rest $sep $opts)
+set rest (echo $rest)
+
+set should_rainbow
+set o_count (math (random)%64+4)
+
+set i 1
+while test $i -le (count $flags)
+    switch $flags[$i]
+        case '-r'
+            set should_rainbow 1
+        case '-c'
+            set i (math $i+1)
+            set o_count $flags[$i]
+    end
+    set i (math $i+1)
+end
+
+
+function o
+    cat /dev/urandom | tr -dc 'oO' | head -c $argv[1]
+end
+
+if test "$o_count" -ge 0
+    if test -n "$should_rainbow"
+        msg $chan (rainbow (o $o_count))
+    else
+        msg $chan (o $o_count)
+    end
+else
+    msg $chan $nick': read a book'
+end
