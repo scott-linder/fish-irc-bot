ba6178eac704de56a385caa4ab93896af5fa4415
scott-linder <scott.b.linder@wmich.edu>
2015-06-27 15:07:37 -0400

    Change $do to accept a channel

--- bin/do.fish
+++ bin/do.fish
@@ -1,1 +1,9 @@
-me $chan $rest
+set -l to_chan (clean_chan (echo $rest | sed -n 's/^\([^ ]\+\) .*$/\1/p'))
+set -l to_do (echo $rest | sed -n 's/^[^ ]\+ \(.*\)/\1/p')
+if test -n "$to_chan" -a -n "$to_do"
+    if sudoer $nick $to_chan
+        me $to_chan $to_do
+    end
+else
+    msg $chan $nick': invalid syntax'
+end
