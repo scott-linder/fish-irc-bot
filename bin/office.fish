832f62e224b2eebd2cb7ca7ab95bd4e2604aa1be
scott-linder <scott.b.linder@wmich.edu>
2015-07-21 17:29:42 -0400

    Add door status to $office

--- bin/office.fish
+++ bin/office.fish
@@ -1,6 +1,6 @@
 . lib/opts.fish
 
-set base_url "http://magpie.dhcp.io"
+set base_url "http://shrek.dhcp.io:5001"
 
 set opts (opts_getopts r:d:l $rest)
 set sep (opts_find_sep $opts)
@@ -27,16 +27,17 @@ while test $i -le (count $flags)
 end
 
 if test -n "$reg"
-    msg $chan $nick': '(curl -d "nick=$nick&mac=$reg" "$base_url/reg")
+    msg $chan $nick': '(curl -m 10 -d "nick=$nick&mac=$reg" "$base_url/reg")
 else if test -n "$dereg"
-    msg $chan $nick': '(curl -d "nick=$nick&mac=$dereg" "$base_url/dereg")
+    msg $chan $nick': '(curl -m 10 -d "nick=$nick&mac=$dereg" "$base_url/dereg")
 else if test -n "$list"
-    msg $chan $nick': '(curl -d "nick=$nick" "$base_url/list")
+    msg $chan $nick': '(curl -m 10 -d "nick=$nick" "$base_url/list")
 else
-    set around (curl "$base_url/plain")
-    if test -n "$around"
-        msg $chan $nick': '$around
-    else
-        msg $chan $nick': no user machines active'
+    set around (curl -m 10 "$base_url/plain")
+    if test -z "$around"
+        set around 'no user machines active'
     end
+    set door (curl -m 10 -k 'https://yakko.cs.wmich.edu:9001/door')
+    set message $around' | '$door
+    msg $chan $nick': '$message
 end
