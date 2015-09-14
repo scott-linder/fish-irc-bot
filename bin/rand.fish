8821a9da24bbf9daabd30d72ea588f80c237b4b1
scott-linder <scott.b.linder@wmich.edu>
2015-09-14 18:48:08 -0400

    Add rainbow to $rand, with $rrand alias

--- bin/rand.fish
+++ bin/rand.fish
@@ -1,5 +1,33 @@
 . lib/file.fish
+. lib/opts.fish
+. lib/color.fish
+
+set opts (opts_getopts r $rest)
+set sep (opts_find_sep $opts)
+set flags (opts_flags $sep $opts)
+set rest (opts_rest $sep $opts)
+
+set should_rainbow
+
+set i 1
+while test $i -le (count $flags)
+    switch $flags[$i]
+        case '-r'
+            set should_rainbow 1
+    end
+    set i (math $i+1)
+end
+
+if test $cmd = rrand
+    set should_rainbow 1
+end
+
 set file (chan_file $chan $rest)
 if test -n "$file" -a -f $file
-    msg $chan (cat $file | sort -R | head -1)
+    set line (cat $file | sort -R | head -1)
+    if test -n "$should_rainbow"
+        msg $chan (rainbow $line)
+    else
+        msg $chan $line
+    end
 end
