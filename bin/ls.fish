4c24ec4a736ada47b8c7e967839627f10c55cb01
scott-linder <scott.b.linder@wmich.edu>
2015-09-14 18:47:18 -0400

    Allow filtering results of $ls

--- bin/ls.fish
+++ bin/ls.fish
@@ -1,7 +1,12 @@
 . lib/file.fish
+. lib/sed.fish
 set dir (chan_dir $chan)
 if test -n "$dir" -a -d $dir
-    msg $chan (ls (chan_dir $chan) | tr \n ' ')
+    if test -n "$rest"
+        msg $chan (ls (chan_dir $chan) | sed -n "/^"(sed_escape_regexp $rest)"/p")
+    else
+        msg $chan (ls (chan_dir $chan) | tr \n ' ')
+    end
 else
     msg $chan $nick': no such directory'
 end
